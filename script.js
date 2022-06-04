b9f8df455a275c78cdf8e19992b1d8f728daa184
Lira Artem <artemlira84@gmail.com>
2022-06-04 14:18:01 +0300

    Fetch

--- script.js
+++ script.js
@@ -25,31 +25,32 @@
 // }
 
 //==================================================================================
-let url = 'https://jsonplaceholder.typicode.com/posts';
+// let url = 'https://jsonplaceholder.typicode.com/posts';
 
-const sendPost = () => {
-   return new Promise((resolve, reject) => {
-      const request = new XMLHttpRequest();
-      request.addEventListener('readystatechange', () => {
-         if (request.readyState !== 4) { return };
-         if (request.status === 200) {
-            let data = JSON.parse(request.responseText);
-            resolve(data);
-         } else {
-            reject('Беда печаль. Получилась ошибка');
-         }
 
-      });
-      request.open('GET', url);
-      request.setRequestHeader('Content-type', 'application/json');
-      request.send();
-   });
-}
+// const sendPost = () => {
+//    return new Promise((resolve, reject) => {
+//       const request = new XMLHttpRequest();
+//       request.addEventListener('readystatechange', () => {
+//          if (request.readyState !== 4) { return };
+//          if (request.status === 200) {
+//             let data = JSON.parse(request.responseText);
+//             resolve(data);
+//          } else {
+//             reject('Беда печаль. Получилась ошибка');
+//          }
 
-sendPost()
-   .then((data) => { createWrap(data) })
-   .catch((error) => { console.error(error) })
-   .finally(() => console.log('Все сделано'));
+//       });
+//       request.open('GET', url);
+//       request.setRequestHeader('Content-type', 'application/json');
+//       request.send();
+//    });
+// }
+
+// sendPost()
+//    .then((data) => { createWrap(data) })
+//    .catch((error) => { console.error(error) })
+//    .finally(() => console.log('Все сделано'));
 
 
 
@@ -70,18 +71,128 @@ sendPost()
 //    request.send();
 // }
 
+// const createWrap = (data) => {
+//    data.forEach((item) => {
+//       if (item.userId == 1) {
+//          let str = `<div class="col-12 col-md-6">
+//                         <div class="wrap">
+//                            ${item}
+//                          </div>
+//                      </div>`;
+//          document.querySelector('.row').insertAdjacentHTML('beforeend', str);
+//       }
+//    });
+// }
+
+// sendPost();
+
+// =========================================================================
+// function arr() {
+//    let arr = [];
+//    let num;
+//    for (let i = 0; arr.length < 6; i++) {
+//       num = Math.floor(Math.random() * (5000 - 1 + 1)) + 1;
+//       if (arr.indexOf(num) == -1) {
+//          arr.push(num);
+//       }
+
+//    }
+//    return arr
+// }
+
+// console.log(arr());
+
+// const createWrap = (data) => {
+//    let str = `<div class="col-12 col-md-6">
+//                         <div class="wrap">
+//                         <img src="${data}" alt="">
+//                          </div>
+//                      </div>`;
+//    document.querySelector('.row').insertAdjacentHTML('beforeend', str);
+// }
+
+// let url = 'https://jsonplaceholder.typicode.com/photos';
+
+// const sendPhoto = () => {
+//    return new Promise((resolve, reject) => {
+//       const request = new XMLHttpRequest();
+//       request.addEventListener('readystatechange', () => {
+//          if (request.readyState !== 4) { return };
+//          if (request.status === 200) {
+//             let data = JSON.parse(request.responseText);
+//             resolve(data);
+//          } else {
+//             reject('Беда печаль. Получилась ошибка');
+//          }
+
+//       });
+//       request.open('GET', url);
+//       request.setRequestHeader('Content-type', 'application/json');
+//       request.send();
+//    });
+// }
+
+// sendPhoto()
+//    .then((data) => {
+//       let arr = [];
+//       // let num;
+//       for (let i = 0; arr.length < 6; i++) {
+//          let num = Math.floor(Math.random() * (5000 - 1 + 1)) + 1;
+//          if (arr.indexOf(num) == -1) {
+//             arr.push(data[num]);
+//          }
+//       }
+//       return arr
+//    })
+//    .then(data => {
+//       console.log(data);
+//       data.forEach((item) => {
+//          console.log(item.url);
+//          createWrap(item.url)
+//       })
+//    })
+//    .catch((error) => { console.error(error) })
+//    .finally(() => console.log('Все сделано'));
+
+//=====================================================================================
+
+let url = 'https://jsonplaceholder.typicode.com/posts';
+
+const sendPost = () => {
+   fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      // body:JSON.stringify(data) //если отправляем данные
+   })
+      .then((response) => {
+         if (response.status !== 200) {
+            throw new Error('Response status is not 200')
+         }
+         return response.text();
+         console.dir(response.text());
+      })
+      .then((res) => {
+         return JSON.parse(res);
+         console.log(res);
+      })
+      .then((data) => {
+         createWrap(data);
+      })
+      .catch((error) => console.error(error));
+}
+
+sendPost();
+
 const createWrap = (data) => {
    data.forEach((item) => {
       if (item.userId == 1) {
          let str = `<div class="col-12 col-md-6">
                         <div class="wrap">
-                           <h2 class ="title">${item.title}</h2>
-                           <p>${item.body}</p>
+                          <h2>${item.title}</h2>
+                          <p>${item.body}</p>
                          </div>
                      </div>`;
          document.querySelector('.row').insertAdjacentHTML('beforeend', str);
       }
    });
 }
-
-// sendPost();
